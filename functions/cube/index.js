744f58b686ba826d29d2efeb8715d14a55b38f49
André Poleza <andrepoleza@nepente.io>
2016-09-05 18:54:50 -0300

    Adds cube function

---
+++ functions/cube/index.js
@@ -0,0 +1,7 @@
+console.log('starting function')
+
+exports.handle = function(e, ctx, cb) {
+  console.log('processing event: %j', e)
+  var number = parseInt(e.number)
+  cb(null, number * number * number)
+}
